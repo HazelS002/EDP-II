558ce406fde12212ef41a87757f0e54f766ab0f6
HazelS002 <hazelshamedsan@gmail.com>
2026-04-17 12:29:03 -0600

    Correccion de time_text

--- scripts/tarea-1/tarea-1.py
+++ scripts/tarea-1/tarea-1.py
@@ -170,40 +170,34 @@ def create_3d_plot(X, T, U, ax, cmap):
     return surf
 
 def create_animation(X, T, U, ax, cmap):
-    """ Animación de la solución como una barra de colores horizontal. El eje x
-    representa x, el eje y es fijo (para dar efecto de barra horizontal), y el
-    color representa u(x,t) en cada instante de tiempo t.
-    Parámetros:
-    - X, T: mallas 2D para x y t respectivamente (shape: (len(x), len(t)))
-    - U: solución calculada (shape: (len(x), len(t)))
-    - ax: objeto de eje de matplotlib donde se dibujará la gráfica
-    - cmap: mapa de colores a usar para la gráfica
-    Retorna: anim, el objeto de la animación. """
     x, t = X[:, 0], T[0, :]
 
-    # eliminar elementos innecesarios para una barra horizontal
+    # limpiar ejes
     for place in ['top', 'right', 'left']: ax.spines[place].set_visible(False)
     ax.set_xlabel(r'$x$'); ax.set_yticks([])
-    
-    img_data = U[:, 0].reshape(1, -1) # matriz de una sola fila (1, len(x))
+
+    # imagen inicial
+    img_data = U[:, 0].reshape(1, -1)
     im = ax.imshow(img_data, extent=[x.min(), x.max(), -0.05, 0.05],
                    aspect='auto', origin='lower', cmap=cmap)
-    ax.set_ylim(-1, 1) # Limitar eje y (Efecto de barra horizontal)
-    ax.set_title(f't={t[0]}') # Título inicial con t=0
-    
-    
+    ax.set_ylim(-1, 1)
+    time_text = ax.text(0.35, 0.7, '', transform=ax.transAxes, fontsize=15)
+
+    def init():
+        im.set_array(U[:, 0].reshape(1, -1))
+        time_text.set_text(rf'$t={t[0]:.4f}$')
+        return [im, time_text]
+
     def update(frame):
         idx = frame % len(t)
-        # Actualizar los datos de la imagen con la columna correspondiente
-        ax.set_title(f't={t[idx]}')
-        new_data = U[:, idx].reshape(1, -1)
-        im.set_array(new_data)
-        return [im]
-    
-    anim = FuncAnimation(ax.figure, update, frames=len(t), interval=INTERVAL,
-                         repeat=REPEAT, blit=True)
-    return anim
+        im.set_array(U[:, idx].reshape(1, -1))
+        time_text.set_text(rf'$t={t[idx]:.4f}$')
+        return [im, time_text]
 
+    anim = FuncAnimation(ax.figure, update, frames=len(t), init_func=init,
+        interval=INTERVAL, repeat=REPEAT, blit=True)
+
+    return anim
 
 if __name__ == "__main__":
     """ Ejecuta los ejercicios de la tarea. Para cada ejercicio, se pueden
