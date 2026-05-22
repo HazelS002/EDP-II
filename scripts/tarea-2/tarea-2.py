62d4a00f4f4811c35f0ab187fbc16bc2a91082d6
HazelS002 <hazelshamedsan@gmail.com>
2026-05-22 17:19:27 -0600

    Excersise 2, scripts/traea-2.py

--- scripts/tarea-2/tarea-2.py
+++ scripts/tarea-2/tarea-2.py
@@ -27,6 +27,7 @@ def equation_a():
     sol = solver.solve(eq)
     print(f"Solución ADM ({N_termns} términos):")
     sp.pprint(sol)
+    return sol
     
 
 def equation_b():
@@ -47,7 +48,8 @@ def equation_b():
     sol = solver.solve(eq)
     print(f"Solución ADM (primeros {N_termns} términos en t):")
     sp.pprint(sol)
-    
+    return sol
+
 def equation_c():
     print_title("(c) EDP: u_t + u^2 u_x = 0, u(x,0)=3x")
     
@@ -66,8 +68,10 @@ def equation_c():
     sol = solver.solve(eq)
     print(f"Solución ADM (primeros {N_termns} términos en t):")
     sp.pprint(sol)
+    return sol
 
 def system_equations_d():
+    print_title("(d) EDO's syatem")
     t = sp.symbols('t')
     u = sp.Function('u')(t)
     v = sp.Function('v')(t)
@@ -109,6 +113,49 @@ def system_equations_d():
     sp.pprint(sol_v)
 
     return sol_u, sol_v
+
+
+def equation_e():
+    print_title("(e) EDP: u_{tt}=u_{xx}")
+    x, t = sp.symbols('x t')
+    n_termns_fourier = 4
+
+    # Condiciones iniciales expandidas en series de Fourier
+    # sin^3(x) = (3/4) sin(x) - (1/4) sin(3x)
+    # sin(2x) ya está en la base
+    a = {1: sp.Rational(3,4), 3: sp.Rational(-1,4)}   # coeficientes para u(x,0)
+    b = {2: 1}                                         # coeficientes para u_t(x,0)
+    
+    u_approx = 0
+    # Para cada modo n que tenga coeficiente no nulo hasta n_max
+    for n in range(1, n_termns_fourier+1):
+        an = a.get(n, 0)
+        bn = b.get(n, 0)
+        if an == 0 and bn == 0: continue
+        
+        # Variable dependiente T_n(t)
+        T = sp.Function(f'T{n}')(t)
+        # Ecuación: T'' + n^2 T = 0
+        L = T.diff(t, t)
+        R = (n**2) * T
+        N = 0
+        g = 0
+
+        # Condiciones iniciales
+        conds = [
+            Condition(var=T, value=an, at_point=0, is_initial=True),
+            Condition(var=T.diff(t), value=bn, at_point=0, is_initial=True)
+        ]
+
+        eq = Equation(L, R, N, g, [t], T, conds)
+        solver = AdomianMethod(n_terms=N_termns, simplify=True)
+        T_series = solver.solve(eq)
+        # Añadir el modo a la solución
+        u_approx += T_series * sp.sin(n*x)
+
+    sp.pprint(u_approx)
+    
+    return sp.simplify(u_approx)
     
 if __name__ == "__main__":
     from eqsolver import Equation, Condition, AdomianMethod,\
@@ -119,3 +166,4 @@ if __name__ == "__main__":
     equation_b()
     equation_c()
     system_equations_d()
+    equation_e()
